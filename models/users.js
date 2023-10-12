e9b92b96a12c8ca202a9b05dc7d3ae938583703f
Angel Armando Gonzalez Rodriguez <kuategonz@gmail.com>
2023-10-12 14:04:48 -0600

    tercer endpoint

--- models/users.js
+++ models/users.js
@@ -13,6 +13,23 @@ const usersModel = {
           users
         WHERE
           id=?
+       `, 
+       addRow:`
+INSERT INTO
+Users(
+   username,
+   email,
+   password,
+   name,
+   lastname,
+   phone_number,
+   role_id,
+   is_active
+
+)
+VALUES(
+   ?,?,?,?,?,?,?,?
+)
        `,
 };
 module.exports = usersModel;
--- routes/users.js
+++ routes/users.js
@@ -1,13 +1,13 @@
 const { Router } = require("express")
-const {usersList,listUserByID}= require('../controllers/users');
+const {usersList,listUserByID, addUser}= require('../controllers/users');
 const router = Router();
 
 //http://localhost:3000/api/v1/users/
 router.get('/',usersList);
 router.get('/:id', listUserByID);
 //router.post('/',usersList);
-//router.put('/',usersList);
-//router.pacht('/',usersList);
+router.put('/',addUser);
+//router.patch('/',usersList);
 //router.detele('/',usersList);
 
 module.exports = router;
